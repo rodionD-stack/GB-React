61e71389a261ea55587a609c1fc427fdcea916a1
Rodion Dudar <rodion.dudar.rd@gmail.com>
2021-07-23 15:03:30 +0300

    add lesson1

---
+++ lesson1/lesson1/gb-app/src/App.js
@@ -0,0 +1,14 @@
+import { Message } from './Mssage'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Message name='GeekBrains'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
