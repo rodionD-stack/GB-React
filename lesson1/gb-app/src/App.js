da40cac66b1b8cb6369bfbd8847fb198d721f3e0
Rodion Dudar <rodion.dudar.rd@gmail.com>
2021-07-25 19:51:36 +0300

    update app js

--- lesson1/gb-app/src/App.js
+++ lesson1/gb-app/src/App.js
@@ -2,10 +2,13 @@ import { Message } from './Mssage'
 import './App.css';
 
 function App() {
+
+  const someName = 'GeekBrains'
+
   return (
     <div className="App">
       <header className="App-header">
-          <Message name='GeekBrains'/>
+          <Message name={someName}/>
       </header>
     </div>
   );
