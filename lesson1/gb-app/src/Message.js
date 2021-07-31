fbdf49b241e65e4547d9b0084c113503eb1f4730
Rodion Dudar <rodion.dudar.rd@gmail.com>
2021-07-31 14:03:42 +0300

    add lesson3

--- lesson1/gb-app/src/Message.js
+++
@@ -1,6 +0,0 @@
-import './Message.css'
-export const Message = (props) => {
-    return (
-        <h1 className="message__heading">Hello, {props.name}</h1>
-    )
-}
--- lesson1/gb-app/src/components/constants.js
+++
@@ -1,4 +0,0 @@
-export const AUTHORS = {
-    human: 'Me',
-    robot: 'Bot'
-};
