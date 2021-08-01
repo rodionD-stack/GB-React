dfba5357553491e8a9872637f92426e1ca914925
Rodion Dudar <rodion.dudar.rd@gmail.com>
2021-08-01 14:06:15 +0300

    change lesson3

--- lesson1/gb-app/src/components/ChatUi/ChatUi.js
+++ lesson1/gb-app/src/components/ChatUi/ChatUi.js
@@ -20,76 +20,49 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const myChats = [
+  {
+    name: 'Mark chat',
+    id: '1',
+    text: "— I'll be in your neighborhood doing errands this…",
+  },
+
+  {
+    name: 'Rodion chat',
+    id: '2',
+    text: "— Wish I could come, but I'm out of town this…",
+  },
+];
+
 export const ChatUi = () => {
   const classes = useStyles();
 
   return (
     <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
+      {myChats.map((c) => (
+        <ListItem alignItems="flex-start" key={c.id}>
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary={c.name}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Ali Connors
+                </Typography>
+                {c.text}
+              </React.Fragment>
+            }
+          />
+        </ListItem> 
+      ))}
       <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Sandra Adams
-              </Typography>
-              {' — Do you have Paris recommendations? Have you ever…'}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
     </List>
   );
 }
